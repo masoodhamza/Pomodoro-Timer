04877724a472d53118ec8bb2316624b58966dae8
Hamza Masood <masood.hamzaa@gmail.com>
2021-04-18 12:52:16 +0500

    code formating

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+//output time
 const timer = document.querySelector("#timer");
+
+//input textboxes
 const workTime = document.querySelector("#worktime");
 const breakTime = document.querySelector("#breaktime");
 const task = document.querySelector("#task");
+
+//completed task list
+const taskList = document.querySelector("ul");
+
+//buttons
 const play = document.querySelector("#play");
 const pause = document.querySelector("#pause");
 const stop = document.querySelector("#stop");
-const taskList = document.querySelector("ul");
 
+//initilizing timers and intervals
 let totalWorkTime = 0;
 
 let workInterval = null;
@@ -18,57 +26,76 @@ let workTimeLeft = workTime.value * 60;
 let initialBreakTime = breakTime.value * 60;
 let breakTimeLeft = breakTime.value * 60;
 
+//buttons will be enabled when some task in taskbox
+// buttons disabled
 play.disabled = true;
 pause.disabled = true;
 stop.disabled = true;
 
-workTime.addEventListener("change", (e) => {
-  timer.innerHTML = e.target.value + ":00";
-  workTimeLeft = e.target.value * 60;
-  initialWorkTime = e.target.value * 60;
-});
-
-breakTime.addEventListener("change", (e) => {
-  timer.innerHTML = e.target.value + ":00";
-  breakTimeLeft = e.target.value * 60;
-  initialBreakTime = e.target.value * 60;
-});
-
+//upon adding task buttons enabled
 task.addEventListener("keyup", () => {
   play.disabled = false;
   pause.disabled = false;
   stop.disabled = false;
 });
 
-//play button
+//if user change worktime/breaktime
+//chaning worktime
+workTime.addEventListener("change", (e) => {
+  timer.innerHTML = e.target.value + ":00";
+  workTimeLeft = e.target.value * 60;
+  initialWorkTime = e.target.value * 60;
+});
+
+//changing breaktime
+breakTime.addEventListener("change", (e) => {
+  timer.innerHTML = e.target.value + ":00";
+  breakTimeLeft = e.target.value * 60;
+  initialBreakTime = e.target.value * 60;
+});
+
+//function to display remaining time
+const displayTimeLeft = (timeLeft) => {
+  const secondsLeft = timeLeft;
+  let result = "";
+  const seconds = secondsLeft % 60;
+  const minutes = parseInt(secondsLeft / 60) % 60;
+
+  function addZeroes(time) {
+    return time < 10 ? `0${time}` : time;
+  }
+
+  result += `${addZeroes(minutes)}:${addZeroes(seconds)}`;
+  return result.toString();
+};
+
+//function to disable inputs
+const disableInputs = () => {
+  //disabling all input textboxes
+  workTime.disabled = true;
+  breakTime.disabled = true;
+  task.disabled = true;
+
+  //hide play button
+  play.classList.add("d-none");
+
+  //show pause/stop buttons
+  pause.classList.remove("d-none");
+  stop.classList.remove("d-none");
+};
+
+//start task
 let startTask = () => {
+  //changing color of time started time
   workTime.parentElement.classList.add("bg-success", "text-light");
   timer.classList.add("text-success");
   breakTime.parentElement.classList.remove("bg-danger", "text-light");
   timer.classList.remove("text-danger");
 
-  workTime.disabled = true;
-  breakTime.disabled = true;
-  task.disabled = true;
+  //disable inputs
+  disableInputs();
 
-  const displayTimeLeft = () => {
-    const secondsLeft = workTimeLeft;
-    let result = "";
-    const seconds = secondsLeft % 60;
-    const minutes = parseInt(secondsLeft / 60) % 60;
-
-    function addZeroes(time) {
-      return time < 10 ? `0${time}` : time;
-    }
-
-    result += `${addZeroes(minutes)}:${addZeroes(seconds)}`;
-    timer.innerText = result.toString();
-  };
-
-  play.classList.add("d-none");
-  pause.classList.remove("d-none");
-  stop.classList.remove("d-none");
-
+  // start timer
   workInterval = setInterval(() => {
     if (workTimeLeft == 0) {
       clearInterval(workInterval);
@@ -78,41 +105,24 @@ let startTask = () => {
     } else {
       workTimeLeft--;
     }
-    displayTimeLeft();
-  }, 10);
+    timer.innerText = displayTimeLeft(workTimeLeft);
+  }, 1000);
 };
 
 play.addEventListener("click", startTask);
 
 // break task
 let breakTask = () => {
+  //changing color of time break time
   workTime.parentElement.classList.remove("bg-success", "text-light");
   timer.classList.remove("text-success");
   breakTime.parentElement.classList.add("bg-danger", "text-light");
   timer.classList.add("text-danger");
 
-  workTime.disabled = true;
-  breakTime.disabled = true;
-  task.disabled = true;
+  //disable inputs
+  disableInputs();
 
-  const displayTimeLeft = () => {
-    const secondsLeft = breakTimeLeft;
-    let result = "";
-    const seconds = secondsLeft % 60;
-    const minutes = parseInt(secondsLeft / 60) % 60;
-
-    function addZeroes(time) {
-      return time < 10 ? `0${time}` : time;
-    }
-
-    result += `${addZeroes(minutes)}:${addZeroes(seconds)}`;
-    timer.innerText = result.toString();
-  };
-
-  play.classList.add("d-none");
-  pause.classList.remove("d-none");
-  stop.classList.remove("d-none");
-
+  //start timer
   breakInterval = setInterval(() => {
     if (breakTimeLeft == 0) {
       clearInterval(breakInterval);
@@ -121,8 +131,8 @@ let breakTask = () => {
     } else {
       breakTimeLeft--;
     }
-    displayTimeLeft();
-  }, 10);
+    timer.innerText = displayTimeLeft(breakTimeLeft);
+  }, 1000);
 };
 
 //pause button
@@ -141,21 +151,8 @@ let completeTask = () => {
   const li = document.createElement("li");
   const secondsLeft = totalWorkTime + initialWorkTime - workTimeLeft;
 
-  const timeConsumed = () => {
-    let result = "";
-    const seconds = secondsLeft % 60;
-    const minutes = parseInt(secondsLeft / 60) % 60;
-
-    function addZeroes(time) {
-      return time < 10 ? `0${time}` : time;
-    }
-
-    result += `${addZeroes(minutes)}:${addZeroes(seconds)}`;
-    return result.toString();
-  };
-  
   li.textContent = `${task.value} : ${
-    secondsLeft > 60 ? timeConsumed() : " < 1 min"
+    secondsLeft > 60 ? displayTimeLeft(secondsLeft) : " < 1 min"
   }`;
 
   taskList.appendChild(li);
